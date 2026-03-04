b5cdf2af4c556b3ff2c4b08524cc749a698e92c5
Harshvardhan <hkamble1@student.gsu.edu>
2026-03-04 16:27:49 -0500

    feat: build HomeScreen with recipe list and navigation

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../data/recipes_data.dart';
+import 'details_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Recipe Book"),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: sampleRecipes.length,
+        itemBuilder: (context, index) {
+          final recipe = sampleRecipes[index];
+
+          return Card(
+            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            elevation: 4,
+            child: ListTile(
+              leading: Image.asset(
+                recipe.imagePath,
+                width: 60,
+                fit: BoxFit.cover,
+              ),
+              title: Text(recipe.name),
+              trailing: const Icon(Icons.chevron_right),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => DetailsScreen(recipe: recipe),
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
