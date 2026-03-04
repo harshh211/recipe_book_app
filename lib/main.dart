f8575cd6321d4fa82b57aa00105f469e40856631
Harshvardhan <hkamble1@student.gsu.edu>
2026-03-04 16:26:35 -0500

    feat: setup main app entry with HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const RecipeBookApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class RecipeBookApp extends StatelessWidget {
+  const RecipeBookApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Recipe Book',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
       ),
+      home: const HomeScreen(),
     );
   }
 }
